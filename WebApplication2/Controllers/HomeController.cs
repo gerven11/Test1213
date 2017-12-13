8351b915ed829920e544b07a3e36af2a39187f78
陳禮群 <gerven11@hotmail.com>
2017-12-13 14:02:59 +0800

    TEST

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication2.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(); 
         }
 
         public ActionResult About()
